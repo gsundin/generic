477c104c5d19959b22f0c4ed06165f172d3655c2
unknown <sundi024@ECE-IEEE07.ad.umn.edu>
2017-04-11 16:10:34 -0500

    edit: More readable format

--- CDAL-CPP/samples/sample_8_trans.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Matrix.h"
-#include <math.h>
-using namespace std;
-int main ( ) {
-    matrix m ( matrix::matrixRead ("../samples/sample_8.data") ) ;
-    cout << m ;
-}
-
